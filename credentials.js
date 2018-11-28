7afedfb222d0fb1d4ed8301095f829b4d4a0cbe5
Kuanysh Tuktubayev <kuanysh.tuktubayev@mail.ru>
2018-11-28 22:56:06 +0600

    reserve extract

--- credentials.js
+++ credentials.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 module.exports = {cookieSecret: 'force ability luke father',};
-=======
-module.exports = {cookieSecret: 'force ability luke father',};
->>>>>>> 892d86e7af0299449d7cdc9ac9d37ffd28840ce8
